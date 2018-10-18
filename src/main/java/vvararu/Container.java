ca5de0909cc878e7916eb434d610a5dc6aa2d55a
vadimvararu <vararu.vadim@gmail.com>
2018-10-18 15:39:53 +0300

    first commit

---
+++ src/main/java/vvararu/Container.java
@@ -0,0 +1,9 @@
+package vvararu;
+
+public class Container {
+
+    private int privateIntVar = 1;
+
+    private int[] privateArray = new int[]{1, 2, 3};
+
+}
---
+++ src/main/java/vvararu/VarHandlesTest.java
@@ -0,0 +1,40 @@
+package vvararu;
+
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.VarHandle;
+
+public class VarHandlesTest {
+
+    public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
+
+        Container container = new Container();
+
+
+        // - Simple read/write access to a private int attribute
+        VarHandle handle = MethodHandles
+                .privateLookupIn(Container.class, MethodHandles.lookup())
+                .findVarHandle(Container.class, "privateIntVar", int.class);
+
+        handle.set(container, 20);
+
+        // setting a value to the private int attribute
+        handle.set(container, 20);
+
+        // getting the value of the private int attribute
+        System.out.println(handle.get(container));
+
+
+
+        //------------------------------------------------------------------------------------
+
+
+        // - Simple read access to a private int[] element
+        VarHandle privateArrayHandle = MethodHandles
+                .privateLookupIn(Container.class, MethodHandles.lookup())
+                .findVarHandle(Container.class, "privateArray", int[].class);
+
+        System.out.println(((int[]) privateArrayHandle.get(container))[1]);
+
+    }
+
+}
